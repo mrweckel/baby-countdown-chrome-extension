af49e875422039fc5afae67cd87afe9a22629fb8
mrweckel <mrweckel@gmail.com>
2015-10-18 21:47:06 -0400

    Adds loading animation

--- app.js
+++ app.js
@@ -1,46 +1,42 @@
-var dueDate = new Date("Nov 30, 2015");
+function countdown() {
+    var dueDate = new Date("Nov 30, 2015");
     dueDateTime = dueDate.getTime();
 
-var days,
-		hours,
-		minutes,
-		seconds;
+    var days,
+        hours,
+        minutes,
+        seconds;
 
-var countdownContainer = document.getElementById('countdown_container'),
-    // dueDateElement = document.getElementById('due_date'),
-    // todaysDateElement = document.getElementById('todays_date'),
-		nodes = countdownContainer.children;
-
-
-setInterval(function(){
-	var currentDate = new Date(),
-	    currentTime = currentDate.getTime();
-			secondsLeft = (dueDateTime - currentTime)/1000;
-
-	days = parseInt(secondsLeft / 86400);
-	secondsLeft = secondsLeft % 86400;
-
-	hours = parseInt(secondsLeft / 3600);
-	secondsLeft = secondsLeft % 3600;
-
-	minutes = parseInt(secondsLeft / 60);
-	seconds = parseInt(secondsLeft % 60);
-
-	// countdown.innerHTML = days + "D " + hours + "H " + minutes + "M " + seconds + "S";
-
-nodes[0].innerHTML = days;
-nodes[1].innerHTML = hours;
-nodes[2].innerHTML = minutes;
-nodes[3].innerHTML = seconds.toString().length < 2 ? "0" + seconds : seconds;
+    var countdownContainer = document.getElementById('countdown_container'),
+        // dueDateElement = document.getElementById('due_date'),
+        // todaysDateElement = document.getElementById('todays_date'),
+        nodes = countdownContainer.children;
 
 
-// dueDateElement.innerHTML = "Due: " + dueDate.toDateString();
-// todaysDateElement.innerHTML = "Today: " + currentDate.toDateString();
+    setInterval(function() {
+        var currentDate = new Date(),
+            currentTime = currentDate.getTime();
+        secondsLeft = (dueDateTime - currentTime) / 1000;
+
+        days = parseInt(secondsLeft / 86400);
+        secondsLeft = secondsLeft % 86400;
+
+        hours = parseInt(secondsLeft / 3600);
+        secondsLeft = secondsLeft % 3600;
+
+        minutes = parseInt(secondsLeft / 60);
+        seconds = parseInt(secondsLeft % 60);
+
+        nodes[0].innerHTML = days;
+        nodes[1].innerHTML = hours;
+        nodes[2].innerHTML = minutes;
+        nodes[3].innerHTML = seconds.toString().length < 2 ? "0" + seconds : seconds;
+
+        countdownContainer.classList.add("active");
 
 
-}, 1000);
+    }, 1000);
 
+}
 
-
-
-
+countdown();
